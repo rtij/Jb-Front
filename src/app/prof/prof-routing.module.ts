c224a539134b8260a71021803aeeba5d79e4479e
rtij <tiavinartjjonathan@gmail.com>
2021-12-13 17:45:48 +0300

    prof update

--- src/app/prof/prof-routing.module.ts
+++ src/app/prof/prof-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfAccComponent } from './prof-acc/prof-acc.component';
 import { ProfComponent } from './prof.component';
 
-const routes: Routes = [{ path: '', component: ProfComponent }];
+const routes: Routes = [
+  {path: '', component: ProfComponent,children:[
+    {path:'',component:ProfAccComponent}
+  ] },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
